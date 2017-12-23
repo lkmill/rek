7a50099ca6ce337babd51040133d851a7869f12f
Linus Miller <lohfu@lohfu.io>
2017-12-23 15:14:00 +0100

    Stop using bluebird promise and set node-fetch as optional dep

--- babel/inject.js
+++ babel/inject.js
@@ -8,27 +8,13 @@ module.exports = function () {
       Program: {
         enter: (path, _ref) => {
           if (_ref.file.opts.filename === 'src/factory.js') {
-            const nodes = [
-              t.variableDeclaration('const', [
-                t.variableDeclarator(t.identifier('fetch'), t.callExpression(t.identifier('require'), [
-                  t.stringLiteral('node-fetch'),
-                ])),
-              ]),
+            const node = t.variableDeclaration('const', [
+              t.variableDeclarator(t.identifier('fetch'), t.callExpression(t.identifier('require'), [
+                t.stringLiteral('node-fetch'),
+              ])),
+            ])
 
-              t.variableDeclaration('const', [
-                t.variableDeclarator(t.identifier('Promise'), t.callExpression(t.identifier('require'), [
-                  t.stringLiteral('bluebird'),
-                ])),
-              ]),
-
-              t.expressionStatement(t.assignmentExpression(
-                '=',
-                t.memberExpression(t.identifier('fetch'), t.identifier('Promise')),
-                t.identifier('Promise'),
-              )),
-            ]
-
-            path.node.body.unshift(...nodes)
+            path.node.body.unshift(node)
           }
         },
       },
