390fb77f69171cdbb68ef4de890aec0f54f5fa7c
Linus Miller <lohfu@lohfu.io>
2017-12-29 12:23:28 +0100

    Update node-fetch import inject in node to import Headers

--- babel/inject.js
+++ babel/inject.js
@@ -8,11 +8,10 @@ module.exports = function () {
       Program: {
         enter: (path, _ref) => {
           if (_ref.file.opts.filename === 'src/factory.js') {
-            const node = t.variableDeclaration('const', [
-              t.variableDeclarator(t.identifier('fetch'), t.callExpression(t.identifier('require'), [
-                t.stringLiteral('node-fetch'),
-              ])),
-            ])
+            const node = t.importDeclaration([
+              t.importDefaultSpecifier(t.identifier('fetch')),
+              t.importSpecifier(t.identifier('Headers'), t.identifier('Headers')),
+            ], t.stringLiteral('node-fetch'))
 
             path.node.body.unshift(node)
           }
