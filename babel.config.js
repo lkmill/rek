925c598f8631cb9d53bc662af50a78c79a20f15c
Linus Miller <lohfu@lohfu.io>
2019-11-04 09:55:20 +0100

    .babelrc > babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,41 @@
+'use strict'
+
+module.exports = {
+  'env': {
+    'node:cjs': {
+      'presets': [
+        ['@babel/env', { 'shippedProposals': true, 'targets': { 'node': 6 } }],
+      ],
+      'plugins': [
+        'add-module-exports',
+      ],
+    },
+
+    'node:esm': {
+      'presets': [
+        ['@babel/env', { 'shippedProposals': true, 'targets': { 'node': 6 }, 'modules': false }],
+      ],
+    },
+
+    'cjs': {
+      'presets': [
+        ['@babel/env', { 'modules': 'commonjs', 'shippedProposals': true }],
+      ],
+      'plugins': [
+        'add-module-exports',
+      ],
+    },
+
+    'esm': {
+      'presets': [
+        ['@babel/env', { 'modules': false, 'shippedProposals': true }],
+      ],
+    },
+
+    'umd': {
+      'presets': [
+        ['@babel/env', { 'modules': false, 'shippedProposals': true }],
+      ],
+    },
+  },
+}
