b243865cb13a90d4f4802938a889c680fc1c98a8
Linus Miller <lohfu@lohfu.io>
2020-01-11 16:26:13 +0100

    Remove node babel envs (not used)

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,6 @@
 
 module.exports = {
   env: {
-    'node:cjs': {
-      presets: [['@babel/env', { shippedProposals: true, targets: { node: 6 } }]],
-      plugins: ['add-module-exports'],
-    },
-
-    'node:esm': {
-      presets: [['@babel/env', { shippedProposals: true, targets: { node: 6 }, modules: false }]],
-    },
-
     cjs: {
       presets: [['@babel/env', { modules: 'commonjs', shippedProposals: true }]],
       plugins: ['add-module-exports'],
