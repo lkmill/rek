12a2ec4d35dd53d3c1a2b4449ba286bfd099d6c5
Linus Miller <lohfu@lohfu.io>
2020-01-12 02:04:51 +0100

    Add typescript types

---
+++ types.d.ts
@@ -0,0 +1,38 @@
+type PlainObject = {
+  [name: string]: any
+}
+
+interface Options extends Omit<RequestInit, 'body'> {
+  body?: string | PlainObject
+  baseUrl?: string
+}
+
+interface API {
+  fetch(input: RequestInfo, init?: RequestInit): Promise<Response>
+  URL: URL
+  Headers: Headers
+}
+
+interface RekResponse
+  extends Pick<Body, 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text'>,
+    Pick<Promise<Response>, 'then'> {
+  run(): Promise<Response>
+}
+
+export interface Rek {
+  (url: string, options?: Options): RekResponse
+  delete(url: string, options?: Options): RekResponse
+  get(url: string, options?: Options): RekResponse
+  head(url: string, options?: Options): RekResponse
+  patch(url: string, body?: any, options?: Options): RekResponse
+  post(url: string, body?: any, options?: Options): RekResponse
+  put(url: string, body?: any, options?: Options): RekResponse
+
+  factory(defaults?: Options, api?: API): Rek
+  extend(newDefaults?: Options, newApi?: API): Rek
+  getArgs(): [Options, API]
+}
+
+declare let rek: Rek
+
+export default rek
